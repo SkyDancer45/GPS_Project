7c09582369e135e8dc36dfd594a27a384b1d0cbf
Kanishk Purohit <kishupurohit1@gmail.com>
2024-06-12 17:26:36 +0530

    checkpoint1

---
+++ Dtos/NewVehicleDtos.cs
@@ -0,0 +1,16 @@
+﻿namespace GPS.Api.Dtos;
+
+public  record class NewVehicleDtos
+{
+
+        public int OwnerId { get; init; }
+        public string Model { get; init; }
+        public string Make { get; init; }
+        public DateOnly Year { get; init; }
+        private decimal XCoordinate;
+        private decimal YCoordinate;
+        private decimal Altitude;
+        
+        
+    
+}
---
+++ Dtos/VehicleDtos.cs
@@ -0,0 +1,25 @@
+﻿
+namespace GPS.Api.Dtos
+{
+    public record VehicleDto
+    {
+        public int VehicleNum { get; init; }
+        public int OwnerId { get; init; }
+        public string Model { get; init; }
+        public string Make { get; init; }
+        public DateOnly Year { get; init; }
+        private decimal XCoordinate;
+        private decimal YCoordinate;
+        private decimal Altitude;
+        
+
+        public VehicleDto(int vehicleNum, int ownerId, string model, string make, DateOnly year)
+        {
+            VehicleNum = vehicleNum;
+            OwnerId = ownerId;
+            Model = model;
+            Make = make;
+            Year = year;
+        }
+    }
+}
